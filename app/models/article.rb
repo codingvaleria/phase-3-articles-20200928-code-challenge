dedc08211c4e93dde87d55bb988b4d34db3d1c89
codingvaleria <codingvaleria@gmail.com>
2022-10-29 15:17:44 +0300

    Add Class method all

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,20 @@
 class Article
 #set reader for variables
     attr_reader :author, :magazine, :title
-
+    @@all = []
     def initialize(author, magazine, title)
         #intialize variables
         @author = author
         @magazine = magazine
         @title = title
+        @@all << self
     end
+
+    def self.all
+        @@all
+    end
+
+
 
 
 end
