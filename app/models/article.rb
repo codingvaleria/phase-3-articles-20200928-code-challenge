300ff2802f0e60cc2f4bfaf5685a7c21516aa5ba
codingvaleria <codingvaleria@gmail.com>
2022-10-29 15:00:32 +0300

    initialize article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article
+#set reader for variables
+    attr_reader :author, :magazine, :title
+
+    def initialize(author, magazine, title)
+        #intialize variables
+        @author = author
+        @magazine = magazine
+        @title = title
+    end
 
 
 end
